aa09506d04ed676c7c808da97f30d8e7c5d77cab
DESKTOP-BHK1VBM\memon <memoncada05@gmail.com>
2025-03-29 11:54:51 -0600

    se arreglaron los problemas de llamados a la base de datos

--- NutriAppBackEnd/NutriAppBackEnd/Program.cs
+++ NutriAppBackEnd/NutriAppBackEnd/Program.cs
@@ -5,16 +5,21 @@ using NutriAppBackEnd.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Configurar conexion a DB 
+// Configurar conexión a la base de datos
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("SupabaseDB")));
 
+// Configurar CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
-        builder => builder.AllowAnyOrigin().AllowAnyOrigin().AllowAnyMethod());
+        builder => builder
+            .AllowAnyOrigin() // Permitir cualquier origen
+            .AllowAnyMethod() // Permitir cualquier método (GET, POST, etc.)
+            .AllowAnyHeader()); // Permitir cualquier encabezado
 });
 
+// Agregar servicios de controladores
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -23,18 +28,32 @@ builder.Services.AddSwaggerGen(options =>
     {
         Title = "Mi API del proyecto",
         Version = "v1.0",
-        Description = "Documentacion de API para Proyecto de clase"
+        Description = "Documentación de API para Proyecto de clase"
     });
-}
-    );
+});
 
+// Agregar servicios personalizados
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<ConsultaService>();
 
 var app = builder.Build();
-app.UseAuthorization();
-app.MapControllers();
-app.UseSwagger();
-app.UseSwaggerUI();
+
+// Usar CORS antes de la autorización y el mapeo de controladores
 app.UseCors("AllowAll");
+
+// Configurar el middleware de autorización
+app.UseAuthorization();
+
+// Mapeo de controladores
+app.MapControllers();
+
+// Configurar Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API del proyecto v1");
+    c.RoutePrefix = string.Empty; // Esto hace que Swagger UI esté disponible en la raíz
+});
+
+// Ejecutar la aplicación
 app.Run();
